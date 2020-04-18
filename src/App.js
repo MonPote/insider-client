dd9a24a0cb261ce6e2461a5954e799fed3f96fa0
Patrick Ear <patrick.ear@scality.com>
2020-04-18 11:40:40 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as Colyseus from 'colyseus.js';
+
+var client = new Colyseus.Client('ws://localhost:2567');
+
 function App() {
+  const [room, setRoom] = useState(null);
+  useEffect(() => {
+    console.log('client', client);
+
+    client
+      .create('my_room')
+      .then((room) => {
+        console.log('joined successfully', room);
+        setRoom(room);
+        room.send({ type: 'PLAY_CARD', test: 'toto' });
+      })
+      .catch((e) => {
+        console.error('join error', e);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +30,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button
+          onClick={() => {
+            console.log('click');
+            room.send({ move: 'left' });
+            room.send({ move: 'right' });
+          }}
+        >
+          Send
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
